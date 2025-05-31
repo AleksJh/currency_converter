a081ba6c07166822f60a703cc4e8b5974258e948
Aleksan.Jhangiryan <lernagorc90@gmail.com>
2025-05-31 14:08:48 +0400

    Refactor: Codes in JSON.
    No more using html format file to parse currency codes. Now it is JSON.

--- src/currency_app/secondary.py
+++ src/currency_app/secondary.py
@@ -1,28 +1,30 @@
-from pathlib import Path
-from bs4 import BeautifulSoup
 import logging
+import json
+from pathlib import Path
 
 
 logger = logging.getLogger(__name__)
 
 
-def parse_currency_codes_from_html(file_name):
-    """ Parses an HTML file with a currency table and returns
-    a set of currency codes. Should be called from main.py if needed."""
-    logging.debug("Started parser function")
+def parse_currency_codes_from_json(file_name: str) -> set[str]:
+    """Parses a JSON file of currency objects and
+        returns a set of currency codes."""
+    logging.debug("Started JSON parser function")
     file_path = Path(__file__).parent / file_name
 
-    with open(file_path, 'r', encoding='utf-8') as f:
-        logging.info(f'{file_name} opened successfully')
-        soup = BeautifulSoup(f.read(), 'html.parser')
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            logging.debug(f"{file_name} opened successfully")
+            data = json.load(f)
+    except FileNotFoundError:
+        logging.exception(f"File not found: {file_name}")
+        return set()
+    except json.JSONDecodeError as e:
+        logging.exception(f"JSON parsing error: {e}")
+        return set()
 
-    codes = set()
-    for td in soup.find_all('td'):
-        code = td.get_text(strip=True).upper()
-        if len(code) == 3 and code.isalpha():
-            codes.add(code)
+    codes = {entry["code"].upper() for entry in data if "code" in entry and
+             len(entry["code"]) == 3}
 
-    if codes:
-        logging.debug('Suitable data available')
-
+    logging.info(f"Parsed {len(codes)} currency codes from JSON")
     return codes
